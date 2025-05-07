fbc25b5516f6d0bac04287d635006df5d3b07ed2
Julien Larzul <julienlrzl@gmail.com>
2025-05-07 09:53:42 +0200

    reset projet

--- src/resources/db/1_create_db.sql
+++
@@ -1,6 +0,0 @@
--- Vérifier si la base existe déjà avant de la créer
-DROP DATABASE IF EXISTS fanfarehub;
-CREATE DATABASE fanfarehub;
-
--- Se connecter à la base créée
-\c fanfarehub;
--- src/resources/db/3_insert_data.sql
+++
@@ -1,3 +0,0 @@
--- Ajouter un utilisateur (Julien Larzul)
-INSERT INTO "user" (nom_utilisateur, email, mot_de_passe, prenom, nom, genre, contrainte_alimentaire)
-VALUES ('julienlrzl', 'julienlrzl@gmail.com', 'hashedpassword', 'Julien', 'Larzul', 'Homme', 'Aucune');
